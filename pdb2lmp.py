b4e582ee460eac207343a53bb4695d3561bb2a1d
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-25 18:58:23 -0700

    .

--- pdb2lmp.py
+++ pdb2lmp.py
@@ -1,10 +1,8 @@
 from  ase.io import read, write
 import ase.io.vasp
 
-atoms = read('mixture.xyz')
+atoms = read('data.pdb')
 #pos   = atoms.get_positions()
 #print(pos) 
 #ase.io.vasp.write_vasp('POSCAR',atoms,label='KShim',direct=False,sort=False)
-write('mixture.vasp', atoms, format='vasp') #, units='real', atom_style='charge', cube=60.)
-
-#https://wiki.fysik.dtu.dk/ase/ase/io/io.html#ase.io.write
+write('mixture.data', atoms, format='lammps-data', units='real', atom_style='charge', cube=60.)
