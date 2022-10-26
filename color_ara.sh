8b2e2fb89cd96fa8f7c72f05c335af9aa22f3143
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-10-26 12:59:50 -0700

    .

---
+++ color_ara.sh
@@ -0,0 +1,24 @@
+LS_COLORS=$LS_COLORS:'di=1;36:*.py=1;38;5;123:*.cif=01;35:*.txt=01;35:*.dat=01;35:*NCAR=1;4;38;5;214:*POINTS=1;4;38;5;214:*POSCAR=1;4;38;5;214:*OTCAR=1;4;38;5;214:*WAVECAR=00;38;5;214:*CHGCAR=00;38;5;214:*CHG=00;38;5;130:*DOSCAR=00;38;5;214:*PROCAR=00;38;5;130:*CONTCAR=1;38;5;214:*OUTCAR=1;38;5;214:*IBZKPT=00;38;5;130:*EIGENVAL=00;38;5;130:*REPORT=00;38;5;130:*XDATCAR=00;38;5;130:*PCDAT=00;38;5;130:*OSZICAR=1;38;5;214:*.out=00;38;5;130:*restart.json=1;4;38;5;214:*.sh=01;35' ; export LS_COLORS
+
+export re='\e[0m' # regular
+export bd='\e[1m' # color + bold
+export it='\e[3m'
+export bi='\e[3m\e[1m'
+export line='\e[4m' # underline
+export strike='\e[9m' # strikethrough
+export dW='\033[37m\e[0m' # default white
+export R='\033[31m' # red
+export G='\033[32m' # green
+export Y='\033[33m' # yellow
+export Bl='\033[34m' # blue
+export Ma='\033[35m' # magenta
+export Cy='\033[36m' # cyan
+export W='\033[37m' # white
+export Gr='\033[90m' # bright black
+export bR='\033[91m' # bright Red
+export bG='\033[92m' # bright Green
+export bY='\033[93m' # bright yellow
+export bBl='\033[94m' # bright blue
+export bMa='\033[95m' # bright magenta
+export bCy='\033[96m' # bright cyan
+export bW='\033[97m' # bright white
