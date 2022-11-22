3a63ab664619e88245c472198ff0973a5e9ca256
jhyeonjung97 <hyeonjung@postech.ac.kr>
2022-11-22 13:00:51 -0800

    .

--- vasp2png.py
+++ vasp2png.py
@@ -1,13 +1,14 @@
 from ase.io import read, write
 from os import listdir
 import subprocess
-#import sys
+from sys import argv
 import os
 
 directory = '/Users/hailey/figure/'
-select = input("which files?: ")
+select = argv[1:]
+files = ' '.join(select)
 here = os.getcwd()
-subprocess.call('cp '+select+' '+directory+' \n cd '+directory+'', shell=True)
+subprocess.call('cp '+files+' '+directory+' \n cd '+directory+'', shell=True)
 x, y, z = input("rotation anple (x, y, z): ").split()
 x = int(x)
 y = int(y)
@@ -16,5 +17,6 @@ for file in listdir(directory):
     if file.endswith(".vasp") or file.endswith(".xyz"):
         filename = file.split('.')[0]
         atoms = read(file)
+        atoms.center()
         write("%s/%s.png" % (directory, filename), atoms, rotation='%dx, %dy, %dz' % (x, y, z))
 subprocess.call('mv '+directory+'/*.png '+here+' \n cd '+here+' \n rm '+directory+'/*', shell=True)
