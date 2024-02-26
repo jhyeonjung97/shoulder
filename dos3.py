ae1ec1c7a66ec4f7e351d9e301ed1c47eef7b49e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-02-26 01:25:49 -0800

    .

--- dos3.py
+++ dos3.py
@@ -201,7 +201,8 @@ with open('OUTCAR', 'r') as file:
 # Open doscar
 dosfile = 'DOSCAR'
 doscar  = Doscar(dosfile, ispin=ispin, lmax=lmax, lorbit=11)  # calculation setting 
-atoms = list(range(15,15))  # calculated atom ordinal
+atoms = ['31']
+# atoms = list(range(15,15))  # calculated atom ordinal
 # print('atoms: ', atoms)
 
 # Set atoms for integration
