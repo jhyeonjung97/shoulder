e141655275071947fec171aab98ab2c607a77898
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-11 21:58:11 -0700

    .

--- dos3lob.py
+++ dos3lob.py
@@ -62,9 +62,9 @@ if args.subset:
             7: 'x(x2-3y2)'
         }
         m = [subset_dict[number] for number in subset_numbers if number in subset_dict]
-    for mm in m:
-        print(orb + '_' + mm, end=' ')
+    print(' '.join([orb + '_' + mm for mm in m]))
 else:
+    print('s')
     m = None
 
 def pdos_column_names(names, ispin):
