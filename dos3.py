da4c2ff6a7519fe7ab184e11b9768d659e3224d5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-11 20:22:20 -0700

    .

--- dos3.py
+++ dos3.py
@@ -189,8 +189,7 @@ class Doscar:
         """
         Returns a subset of the projected density of states array.
         """
-        valid_m_values = {'s': [' '],
-                          'p': ['y', 'z', 'x'],
+        valid_m_values = {'p': ['y', 'z', 'x'],
                           'd': ['xy', 'yz', 'z2-r2', 'xz', 'x2-y2'],
                           'f': ['y(3x2-y2)', 'xyz', 'yz2', 'z3', 'xz2', 'z(x2-y2)', 'x(x2-3y2)']}
         if not atoms:
