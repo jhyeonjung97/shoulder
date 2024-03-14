4dd80eaccaa1f1315ed7f5a92217b8a711f692bd
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-14 11:21:10 -0700

    .

--- dos3.py
+++ dos3.py
@@ -338,17 +338,17 @@ elif ispin == 2:
     e_num1 = occ1 / total1 * o_num_up * 2
     e_num2 = occ2 / total2 * o_num_down * 2
     e_num = occ / total * o_num_up * 2
-    # print('   occ_up  : {:.4f}\n'.format(occ1),
-    #       'unocc_up  : {:.4f}\n'.format(unocc1),
-    #       'total_up  : {:.4f}\n'.format(total1),
-    #       'e_num_up  : {:.4f} (e-)\n'.format(e_num1))
-    # print('   occ_down: {:.4f}\n'.format(occ2),
-    #       'unocc_down: {:.4f}\n'.format(unocc2),
-    #       'total_down: {:.4f}\n'.format(total2),
-    #       'e_num_down: {:.4f} (e-)\n'.format(e_num2))
-    print('  dbc  : {:.4f} eV'.format(dbc))
-    if not 1.9 * o_num_up * 2 < total < 2.1 * o_num_up * 2:
-        print('  #occ : {:.4f}\n'.format(occ),
-              '#unocc : {:.4f}\n'.format(unocc),
-              '#total : {:.4f}'.format(total))
-    print('  occ  : {:.4f} e-'.format(e_num))
+    print('   occ_up  : {:.4f}\n'.format(occ1),
+          'unocc_up  : {:.4f}\n'.format(unocc1),
+          'total_up  : {:.4f}\n'.format(total1),
+          'e_num_up  : {:.4f} (e-)\n'.format(e_num1))
+    print('   occ_down: {:.4f}\n'.format(occ2),
+          'unocc_down: {:.4f}\n'.format(unocc2),
+          'total_down: {:.4f}\n'.format(total2),
+          'e_num_down: {:.4f} (e-)\n'.format(e_num2))
+    # print('  dbc  : {:.4f} eV'.format(dbc))
+    # if not 1.9 * o_num_up * 2 < total < 2.1 * o_num_up * 2:
+    #     print('  #occ : {:.4f}\n'.format(occ),
+    #           '#unocc : {:.4f}\n'.format(unocc),
+    #           '#total : {:.4f}'.format(total))
+    # print('  occ  : {:.4f} e-'.format(e_num))
