2b36f4d4f1c915adadef2725f7e8e951fcf24ff5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-03-14 11:25:50 -0700

    .

--- .ipynb_checkpoints/dos3-checkpoint.py
+++ .ipynb_checkpoints/dos3-checkpoint.py
@@ -338,11 +338,13 @@ elif ispin == 2:
     e_num1 = occ1 / total1 * o_num_up * 2
     e_num2 = occ2 / total2 * o_num_down * 2
     e_num = occ / total * o_num_up * 2
-    print('   occ_up  : {:.4f}\n'.format(occ1),
+    print('   dbc_up  : {:.4f} (eV)\n'.format(dbc_up),
+          '  occ_up  : {:.4f}\n'.format(occ1),
           'unocc_up  : {:.4f}\n'.format(unocc1),
           'total_up  : {:.4f}\n'.format(total1),
           'e_num_up  : {:.4f} (e-)\n'.format(e_num1))
-    print('   occ_down: {:.4f}\n'.format(occ2),
+    print('   dbc_down: {:.4f} (eV)\n'.format(dbc_down),   
+          '  occ_down: {:.4f}\n'.format(occ2),
           'unocc_down: {:.4f}\n'.format(unocc2),
           'total_down: {:.4f}\n'.format(total2),
           'e_num_down: {:.4f} (e-)\n'.format(e_num2))
