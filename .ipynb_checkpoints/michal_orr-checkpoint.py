07656f68d90f73db2b12cc26c3ccaf6f3cc71286
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-26 15:52:38 -0700

    .

--- .ipynb_checkpoints/michal_orr-checkpoint.py
+++ .ipynb_checkpoints/michal_orr-checkpoint.py
@@ -111,7 +111,7 @@ for idx, row in df.iterrows():
 
 # Add scaling line
 ax.plot(x, x+3.2, '--', lw=1, dashes=(3, 1), c='black')
-ax.text(1.0, 2.2, r'$\Delta$G$_{\sf OOH}$=$\Delta$G$_{\sf OH}$+3.2 eV', color='black', fontsize=10)
+ax.text(1.0, 2.1, r'$\Delta$G$_{\sf OOH}$=$\Delta$G$_{\sf OH}$+3.2 eV', color='black', fontsize=10)
 ax.legend(bbox_to_anchor=(0.5, 1.15), loc='center', borderaxespad=0.5, ncol=3, fancybox=True, shadow=False, fontsize='x-small', handlelength=2)
 fig.savefig('contour_ORR.png', bbox_inches='tight')
 fig.clf()
