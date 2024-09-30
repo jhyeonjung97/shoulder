6f95f67fa0f18eccac435a0d0cf5d692cbf6030a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-09-30 16:35:31 -0700

    .

--- .ipynb_checkpoints/michal_orr-all-checkpoint.py
+++ .ipynb_checkpoints/michal_orr-all-checkpoint.py
@@ -130,12 +130,12 @@ color_ranges = [
 
 # Plot the general dataset points
 for row_num, row in enumerate(df.itertuples(), 1):  # Start row number from 1
-    ax.scatter(row.dG_OH, row.dG_OOH, label=f'{row.Index}: {row.overpotential:.2f} V',
-               # marker=markers[row_num-1],
-               s = 24, marker='o',
+    ax.scatter(row.dG_OH, row.dG_OOH, label=f'{row.Index}: {row.overpotential:.2f} V',               
+               s = 24, marker='o', # marker=markers[row_num-1],
                linewidths=0.5, # Use row_num for marker cycling
                facecolors=colors[row_num-1],  # White fill for contrast (use facecolors for scatter)
-               edgecolors=colors[row_num-1])  # Black edge color
+               edgecolors=black,
+               zorder = 10)  # Black edge color
 
 # Plot the metal-specific data points with colormaps
 for m, metal in enumerate(metals):
