2d61583e602b9698af985f4f07c6fa0592db9bf5
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-01 15:00:13 -0700

    .

--- michal_oer.py
+++ michal_oer.py
@@ -45,9 +45,9 @@ setfont()
 
 # Plot settings
 ax = fig.add_axes([0.2, 0.2, 0.6, 0.6])
-xcenter, ycenter = 1.5, 1.5
-x1, x2 = xcenter - 1.5, xcenter + 1.5 # 3.2
-y1, y2 = ycenter - 2.5, ycenter + 1.0 # 3.6
+xcenter, ycenter = 1.60, 0.81
+x1, x2 = xcenter - 1.0, xcenter + 1.0 # 3.2
+y1, y2 = ycenter - 1.8, ycenter + 1.8 # 3.6
 
 ax.axis([x1, x2, y1, y2])
 ax.set_xlabel(r'$\Delta$G$_{\sf O}$ - $\Delta$G$_{\sf OH}$(eV)', fontsize=10)
