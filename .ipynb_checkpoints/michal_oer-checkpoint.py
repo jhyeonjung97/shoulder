c1a1496d5b8811287b553e3b307e7bf39e63ea3a
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-10-03 14:18:00 -0700

    .

--- .ipynb_checkpoints/michal_oer-checkpoint.py
+++ .ipynb_checkpoints/michal_oer-checkpoint.py
@@ -151,11 +151,15 @@ for m, metal in enumerate(metals):
 
 # ax.plot(x, x + 3.2, '--', lw=1, dashes=(3, 1), c='black')
 # ax.text(1.1, 2.3, r'$\Delta$G$_{\sf OOH}$=$\Delta$G$_{\sf OH}$+3.2 eV', color='black', fontsize=10)
-for m, metal in enumerate(['Mn', 'Fe', 'Co', 'Ni']):
-    overpotential = dfs[metal]['overpotential']
-    ax.scatter([], [], label=f'{metal}: {overpotential:.2f} V', 
-               s=24, marker='X', linewidths=0.5, 
-               facecolor=colors[m], edgecolor='black')
+for row_num, row in enumerate(df.itertuples(), 1):
+    if row_num < 5
+        ax.scatter(row.dG_O - row.dG_OH, row.dG_OH, 
+                   label=f'{row.Index}: {row.overpotential:.2f} V',               
+                   s=24, marker='X', 
+                   linewidths=0.5,
+                   facecolor=colors[row_num-1],
+                   edgecolor='black',
+                   zorder=10)
 ax.scatter([], [], label='relaxed', s=24, marker='X', linewidths=0.5, facecolor='black', edgecolor='black')
 ax.scatter([], [], label='z-fixed', s=24, marker='o', linewidths=0.5, facecolor='black', edgecolor='black')
 
