95a55ed8d38e9a95c4078930b3af62ba47892456
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-04 14:57:32 -0800

    .

--- michal_pourbaix2.py
+++ michal_pourbaix2.py
@@ -254,8 +254,8 @@ for dir in dirs:
     plt.plot(pH2, onsetpotential_oho - pH2 * const, '--', color='darkorange', lw=1, dashes=(3, 1))
     plt.plot(pH2, onsetpotential_ooh - pH2 * const, '--', color='lime', lw=1, dashes=(3, 1))
     ax.text(0.2, 1.23 - 0.35, r'2H$_2$O $\leftrightarrow$ 4H$^+$ + O$_2$ + 4e$^-$', color='blue', rotation=-9.5, fontsize=10)
-    ax.text(7.2, onsetpotential_oho - 0.70, f'$S_8 (*OH+*O): {overpotential_oho:.2f} eV$', color='darkorange', rotation=-9.5, fontsize=10)
-    ax.text(7.2, onsetpotential_ooh - 0.65, f'$S_9 (*OOH): {overpotential_ooh:.2f} eV$', color='lime', rotation=-9.5, fontsize=10)
+    ax.text(7.7, onsetpotential_oho - 0.70, f'$S_8 (*OH+*O): {overpotential_oho:.2f} eV$', color='darkorange', rotation=-9.5, fontsize=10)
+    ax.text(7.7, onsetpotential_ooh - 0.64, f'$S_9 (*OOH): {overpotential_ooh:.2f} eV$', color='lime', rotation=-9.5, fontsize=10)
     plt.legend(loc='lower left', bbox_to_anchor=(0.0, 1.02), # borderaxespad=17, 
                ncol=1, labelspacing=0.3, handlelength=2, fontsize=10,
                fancybox=True, shadow=True)
