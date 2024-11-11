445c33b6d0cd1ea9933470d21d7b57fe6218c91c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-11 15:06:30 -0800

    .

--- michal_pourbaix2.py
+++ michal_pourbaix2.py
@@ -345,7 +345,10 @@ for dir in dirs:
         plt.plot(pH2, overpotentials['onsetP'][2] - pH2 * const, '--', color='lime', lw=1, dashes=(3, 1))
         plt.plot(pH2, overpotentials['onsetP'][3] - pH2 * const, '--', color='cyan', lw=1, dashes=(3, 1))
         ax.text(0.2, 0.65, r'2H$_2$O $\leftrightarrow$ 4H$^+$ + O$_2$ + 4e$^-$', color='blue', rotation=-9.5, fontsize=10)
-        ax.text(7.7, overpotentials['onsetP'][0] - 0.65, f"*$\rightarrow$*OH$\rightarrow$*O$\rightarrow$*OOH: {overpotentials['overP'][1]:.2f} eV", color='red', rotation=-9.5, fontsize=10)
+        ax.text(7.7, overpotentials['onsetP'][0] - 0.65, 
+                r"*$\rightarrow$*OH$\rightarrow$*O$\rightarrow$*OOH: {overpotentials['overP'][1]:.2f} eV", 
+                # r"$\rightarrow$ OH $\rightarrow$ O $\rightarrow$ OOH: {overpotentials['overP'][1]:.2f} eV", 
+                color='red', rotation=-9.5, fontsize=10)
         ax.text(7.7, overpotentials['onsetP'][1] - 0.96, f"$S_8$ (*OH+*O): {overpotentials['overP'][1]:.2f} eV", color='darkorange', rotation=-9.5, fontsize=10)
         ax.text(7.7, overpotentials['onsetP'][2] - 0.65, f"$S_9$ (*OOH): {overpotentials['overP'][2]:.2f} eV", color='lime', rotation=-9.5, fontsize=10)
         ax.text(7.7, overpotentials['onsetP'][3] - 0.65, f"$S_9$ (*OOH): {overpotentials['overP'][3]:.2f} eV", color='cyan', rotation=-9.5, fontsize=10)
