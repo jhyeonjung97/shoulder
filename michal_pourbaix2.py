6411488748b635da515520183db6989b703f689c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-11 15:29:23 -0800

    .

--- michal_pourbaix2.py
+++ michal_pourbaix2.py
@@ -355,16 +355,16 @@ for dir in dirs:
         plt.plot(pH2, overpotentials['onsetP'][2] - pH2 * const, '--', color='lime', lw=1, dashes=(3, 1))
         plt.plot(pH2, overpotentials['onsetP'][3] - pH2 * const, '--', color='violet', lw=1, dashes=(3, 1))
         ax.text(0.2, 0.65, r'2H$_2$O $\leftrightarrow$ 4H$^+$ + O$_2$ + 4e$^-$', color='blue', rotation=-9.5, fontsize=10)
-        ax.text(6.6, overpotentials['onsetP'][0] - 0.94, 
+        ax.text(6.6, overpotentials['onsetP'][0] - 0.93, 
                 r"S$_0$$\rightarrow$S$_2$$\rightarrow$S$_3$$\rightarrow$S$_6$: " + f"{overpotentials['overP'][0]:.2f} eV", 
                 color='red', rotation=-9.5, fontsize=10)
-        ax.text(6.6, overpotentials['onsetP'][1] - 1.14, 
+        ax.text(6.6, overpotentials['onsetP'][1] - 1.13, 
                 r"S$_2$$\rightarrow$S$_3$$\rightarrow$S$_8$$\rightarrow$S$_{10}$: " + f"{overpotentials['overP'][1]:.2f} eV", 
                 color='darkorange', rotation=-9.5, fontsize=10)
-        ax.text(6.6, overpotentials['onsetP'][2] - 0.74, 
+        ax.text(6.6, overpotentials['onsetP'][2] - 0.73, 
                 r"S$_3$$\rightarrow$S$_8$$\rightarrow$S$_9$$\rightarrow$S$_{11}$: " + f"{overpotentials['overP'][2]:.2f} eV", 
                 color='lime', rotation=-9.5, fontsize=10)
-        ax.text(6.6, overpotentials['onsetP'][3] - 0.98,
+        ax.text(6.6, overpotentials['onsetP'][3] - 0.97,
                 r"S$_3$$\rightarrow$S$_8$$\rightarrow$S$_{10}$$\rightarrow$S$_{11}$: " + f"{overpotentials['overP'][3]:.2f} eV", 
                 color='violet', rotation=-9.5, fontsize=10)
     elif A == '2' and B == 'Co':
