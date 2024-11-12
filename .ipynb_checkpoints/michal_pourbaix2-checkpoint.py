71a8a0406ee04f565f93e4d50d71dc02bd334ab2
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-11-12 15:24:45 -0800

    .

--- .ipynb_checkpoints/michal_pourbaix2-checkpoint.py
+++ .ipynb_checkpoints/michal_pourbaix2-checkpoint.py
@@ -367,26 +367,30 @@ for dir in dirs:
         #         r"S$_6$$\rightarrow$S$_{10}$$\rightarrow$S$_{11}$$\rightarrow$S$_{12}$: " + f"{overpotentials['overP'][3]:.2f} eV", 
         #         color='orange', rotation=-9.5, fontsize=10)
         ax.text(6.5, overpotentials['onsetP'][7] - 0.96, # 1.14, 
-                r"S$_6$$\rightarrow$S$_{10}$$\rightarrow$S$_{11}$$\rightarrow$S$_{12}$: " + f"{overpotentials['overP'][7]:.2f} eV", 
+                r"S$_3$$\rightarrow$S$_8$$\rightarrow$S$_{10}$$\rightarrow$S$_{11}$: " + f"{overpotentials['overP'][7]:.2f} eV", 
                 color='lime', rotation=-9.5, fontsize=10)
     elif A == '2' and B == 'Co':
         plt.plot(pH2, overpotentials['onsetP'][0] - pH2 * const, '--', color='black', lw=1, dashes=(3, 1))
-        plt.plot(pH2, overpotentials['onsetP'][1] - pH2 * const, '--', color='red', lw=1, dashes=(3, 1))
-        plt.plot(pH2, overpotentials['onsetP'][2] - pH2 * const, '--', color='green', lw=1, dashes=(3, 1))
-        plt.plot(pH2, overpotentials['onsetP'][3] - pH2 * const, '--', color='orange', lw=1, dashes=(3, 1))
+        plt.plot(pH2, overpotentials['onsetP'][1] - pH2 * const, '--', color='violet', lw=1, dashes=(3, 1))
+        # plt.plot(pH2, overpotentials['onsetP'][2] - pH2 * const, '--', color='green', lw=1, dashes=(3, 1))
+        # plt.plot(pH2, overpotentials['onsetP'][3] - pH2 * const, '--', color='orange', lw=1, dashes=(3, 1))
+        plt.plot(pH2, overpotentials['onsetP'][5] - pH2 * const, '--', color='red', lw=1, dashes=(3, 1))
         ax.text(0.2, 0.65, r'2H$_2$O $\leftrightarrow$ 4H$^+$ + O$_2$ + 4e$^-$', color='blue', rotation=-9.5, fontsize=10)
         ax.text(6.5, overpotentials['onsetP'][0] - 0.72, 
                 r"S$_0$$\rightarrow$S$_2$$\rightarrow$S$_3$$\rightarrow$S$_6$: " + f"{overpotentials['overP'][0]:.2f} eV", 
                 color='black', rotation=-9.5, fontsize=10)
         ax.text(6.5, overpotentials['onsetP'][1] - 0.54,
                 r"S$_2$$\rightarrow$S$_5$$\rightarrow$S$_8$$\rightarrow$S$_{10}$: " + f"{overpotentials['overP'][1]:.2f} eV", 
+                color='violet', rotation=-9.5, fontsize=10)
+        # ax.text(6.5, overpotentials['onsetP'][2] - 0.94, 
+        #         r"S$_3$$\rightarrow$S$_8$$\rightarrow$S$_9$$\rightarrow$S$_{11}$: " + f"{overpotentials['overP'][2]:.2f} eV", 
+        #         color='green', rotation=-9.5, fontsize=10)
+        # ax.text(6.5, overpotentials['onsetP'][3] - 0.72, 
+        #         r"S$_6$$\rightarrow$S$_{10}$$\rightarrow$S$_{11}$$\rightarrow$S$_{12}$: " + f"{overpotentials['overP'][3]:.2f} eV", 
+        #         color='orange', rotation=-9.5, fontsize=10)
+        ax.text(6.5, overpotentials['onsetP'][5] - 0.54,
+                r"S$_2$$\rightarrow$S$_3$$\rightarrow$S$_8$$\rightarrow$S$_{10}$: " + f"{overpotentials['overP'][5]:.2f} eV", 
                 color='red', rotation=-9.5, fontsize=10)
-        ax.text(6.5, overpotentials['onsetP'][2] - 0.94, 
-                r"S$_3$$\rightarrow$S$_8$$\rightarrow$S$_9$$\rightarrow$S$_{11}$: " + f"{overpotentials['overP'][2]:.2f} eV", 
-                color='green', rotation=-9.5, fontsize=10)
-        ax.text(6.5, overpotentials['onsetP'][3] - 0.72, 
-                r"S$_6$$\rightarrow$S$_{10}$$\rightarrow$S$_{11}$$\rightarrow$S$_{12}$: " + f"{overpotentials['overP'][3]:.2f} eV", 
-                color='orange', rotation=-9.5, fontsize=10)
     elif A == '3' and B == 'Mo':
         plt.plot(pH2, overpotentials['onsetP'][2] - pH2 * const, '--', color='brown', lw=1, dashes=(3, 1))
         ax.text(0.2, 0.88, r'2H$_2$O $\leftrightarrow$ 4H$^+$ + O$_2$ + 4e$^-$', color='blue', rotation=-9.5, fontsize=10)
