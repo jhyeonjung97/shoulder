cab23cf187207e1cf1898012fa2860904ff0cb9e
jhyeonjung97 <hyeonjung@postech.ac.kr>
2024-12-23 03:35:27 -0800

    .

--- michal_pourbaix2.py
+++ michal_pourbaix2.py
@@ -103,9 +103,9 @@ dsoh = dgoh - (dgh2o - 0.5 * dgh2)
 dsooh = dgooh - (2 * dgh2o - 1.5 * dgh2)
 dsh = dsoh - dso
 
-color = ['turquoise', 'green', 'red', 'pink', 'mediumpurple', 
-         'blue', 'gold', 'lime', 'darkorange', 'yellowgreen',
-         'olive', 'gray', 'violet', 'navy', 'purple', 
+color = ['turquoise', 'green', 'red', 'pink', 'gray', 
+         'blue', 'gold', 'lime', 'darkorange', 'mediumpurple',
+         'olive', 'yellowgreen', 'violet', 'navy', 'purple', 
          'teal', 'deeppink', 'cyan', 'dodgerblue', 'steelblue', 
          'darkslategrey']
 pH2 = np.arange(0, 14.01, 0.01)
