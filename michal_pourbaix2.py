53cbfcb3c04db4a70530e92065d229bc13ac347c
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-01-14 00:09:59 -0800

    .

--- michal_pourbaix2.py
+++ michal_pourbaix2.py
@@ -195,20 +195,21 @@ def dg(i, x, y):
     if surfs[i][0] is None:
         return None
     elif i == 0 and surfs[i][1] == 2:
-        print(surfs[i][0]) 
-        print(surfs[1][0])
-        print(cation)
-        print(charge)
-        print(x)
-        print(const)
-        print(surfs[i][1])
-        print(surfs[i][2])
-        print(surfs[i][3])
-        print(surfs[i][4])
-        print(addH(x, y))
-        print(addO(x, y))
-        print(addOH(x, y))
-        print(addOOH(x, y))
+        if B == 'Co':
+            print(surfs[i][0]) 
+            print(surfs[1][0])
+            print(cation)
+            print(charge)
+            print(x)
+            print(const)
+            print(surfs[i][1])
+            print(surfs[i][2])
+            print(surfs[i][3])
+            print(surfs[i][4])
+            print(addH(x, y))
+            print(addO(x, y))
+            print(addOH(x, y))
+            print(addOOH(x, y))
         return (surfs[i][0] 
                 - surfs[1][0] 
                 + cation - charge * (y + x * const)
