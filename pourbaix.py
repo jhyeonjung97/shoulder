5a118e693e7a81c457f3c08c70a338bf7969f7aa
jhyeonjung97 <hyeonjung@postech.ac.kr>
2025-01-21 00:37:53 -0800

    .

--- pourbaix.py
+++ pourbaix.py
@@ -25,6 +25,10 @@ mpr = MPRester(API_KEY)
 # Define constants
 kJmol = 96.485
 
+df = pd.read_csv('/pscratch/sd/j/jiuy97/6_MNC/figures/pourbaix/1Fe_energies.tsv', delimiter='\t', index_col=0)
+df.index = df.index.str.upper()
+print(df.head())
+
 def get_pourbaix_energy(comp)
     
     SAC_formation_energy=0.1+2*(-0.44)  -2*237/kJmol
